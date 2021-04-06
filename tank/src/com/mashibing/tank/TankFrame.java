5988566217787b899cf026d39bfa51feaf3af77e
wangbing <wb854262177@163.com>
2021-04-06 09:12:18 +0800

    Create 测试.txt

--- tank/src/com/mashibing/tank/TankFrame.java
+++ tank/src/com/mashibing/tank/TankFrame.java
@@ -5,20 +5,45 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class TankFrame extends Frame{
+    private int x=50,y=50;
+
+
     public TankFrame(){
         setTitle("tank war I");
         setResizable(true);
         setVisible(true);
         setSize(800,600);
+        //setBackground(Color.white);
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing (WindowEvent e){
                 System.exit(0);
             }
         });
+        new Thread(new paintThread()).start();
     }
     @Override
     public void paint(Graphics g){
-        g.fillRect(200,200,50,50);
+        g.setColor(Color.green);
+        g.fillOval(x,y,50,50);
+        //g.fillRect(x,y,50,50);
+
+        y+=10;
     }
+
+    public class paintThread implements Runnable{
+        @Override
+        public void run() {
+            while (true){
+                repaint();
+                try {
+                    Thread.sleep(100);
+                }
+                catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
